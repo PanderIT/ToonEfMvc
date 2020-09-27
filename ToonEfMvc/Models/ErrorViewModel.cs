0f31350f4280812f8e539f5d3a296136cdde245a
PanderIT <FreydenIT@gmail.com>
2020-09-26 21:38:50 -0700

    Lab3
    Done

---
+++ ToonEfMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToonEfMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ToonEfMvc/Models/Toons/Cities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToonEfMvc.Models.Toons
+{
+    public partial class Cities
+    {
+        public string CityName { get; set; }
+        public int ProvinceId { get; set; }
+
+        public virtual Provinces Province { get; set; }
+    }
+}
